ec02cd11182917798fcb131938c7d94350396fb4
sbattistelli@qubeyond.com <sbattistelli@qubeyond.com>
2026-05-10 12:49:17 -0300

    chore: untrack framework-testing/ (already in .gitignore)"

--- framework-testing/react/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-
-// Register WCC components
-import './wcc-card.js'
-import './wcc-list.js'
-
-createRoot(document.getElementById('root')).render(<App />)
